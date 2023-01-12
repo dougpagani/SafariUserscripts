70be0bbe5199c669d73d5f7ae118d2f1aa216882
Douglas Pagani <redstonefreedom@gmail.com>
2023-01-12 01:47:25 -0500

    init with examples

---
+++ example-js-userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Example JS Userscript
+// @description This a standard userscript with javascript code
+// @match       https://github.com/quoid/userscripts
+// @exclude-match: *://*.*
+// @version 1.0
+// @updateURL https://www.k21p.com/example.user.js
+// ==/UserScript==
+
+console.log("I am an example userscript");
+
+alert("here i am")
